b6048e11548cbb3fdc896a6f49162aa821a4406e
sofiykarpovich <sofiykarpovich@icloud.com>
2025-11-10 15:49:41 +0300

    Add files via upload

--- include/MessageType.h
+++ include/MessageType.h
@@ -1,1 +1,25 @@
+#pragma once
 
+#include <string>
+#include <string_view>
+#include <iosfwd>
+
+namespace messaging {
+
+enum class MessageType {
+System,
+Server,
+Session
+};
+
+[[nodiscard]] constexpr std::string ToString(MessageType type) noexcept;
+[[nodiscard]] bool FromString(std::string_view text, MessageType& out) noexcept;
+
+[[nodiscard]] inline std::string Serialize(MessageType type) noexcept { return ToString(type); }
+[[nodiscard]] inline bool Deserialize(std::string_view text, MessageType& out) noexcept { return FromString(text, out); }
+
+
+std::ostream& operator<<(std::ostream& os, MessageType type);
+std::istream& operator>>(std::istream& is, MessageType& type);
+
+} // namespace messaging
